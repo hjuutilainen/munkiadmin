81e9f62cfcc5e00d2a966496586d3052ea097b89
Hannes Juutilainen <hjuutilainen@mac.com>
2010-12-15 15:50:12 +0200

    Small GUI and code fixes.

--- PackageInfosArrayController.m
+++ PackageInfosArrayController.m
@@ -14,9 +14,9 @@
 {
 	[super awakeFromNib];
 	
-	NSSortDescriptor *sortByTitle = [[[NSSortDescriptor alloc] initWithKey:@"package.munki_display_name" ascending:YES selector:@selector(localizedStandardCompare:)] autorelease];
-	NSSortDescriptor *sortByVersion = [[[NSSortDescriptor alloc] initWithKey:@"package.munki_version" ascending:YES selector:@selector(localizedStandardCompare:)] autorelease];
-	[self setSortDescriptors:[NSArray arrayWithObjects:sortByTitle, sortByVersion, nil]];
+	NSSortDescriptor *sortByMunkiName = [NSSortDescriptor sortDescriptorWithKey:@"package.munki_name" ascending:YES selector:@selector(localizedStandardCompare:)];
+	NSSortDescriptor *sortByVersion = [NSSortDescriptor sortDescriptorWithKey:@"package.munki_version" ascending:YES selector:@selector(localizedStandardCompare:)];
+	[self setSortDescriptors:[NSArray arrayWithObjects:sortByMunkiName, sortByVersion, nil]];
 	
 	// Reload the table view when catalogs change
 	[self addObserver:self forKeyPath:@"arrangedObjects.isEnabledForCatalog" options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld) context:nil];
