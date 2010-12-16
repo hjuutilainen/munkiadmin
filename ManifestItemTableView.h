e4f67fdd5f5e5da7695be8ee9feb7873c9a1ff3b
Hannes Juutilainen <hjuutilainen@mac.com>
2010-12-16 10:43:32 +0200

    Fixed right-click behaviour in manifest lists. You can now enable/disable all items or selection correctly.
    modified:   English.lproj/MainMenu.xib
    new file:   ManifestItemArrayController.h
    new file:   ManifestItemArrayController.m
    new file:   ManifestItemTableView.h
    new file:   ManifestItemTableView.m

---
+++ ManifestItemTableView.h
@@ -0,0 +1,15 @@
+//
+//  ManifestItemTableView.h
+//  MunkiAdmin
+//
+//  Created by Hannes Juutilainen on 16.12.2010.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ManifestItemTableView : NSTableView {
+
+}
+
+@end
---
+++ ManifestItemTableView.m
@@ -0,0 +1,31 @@
+//
+//  ManifestItemTableView.m
+//  MunkiAdmin
+//
+//  Created by Hannes Juutilainen on 16.12.2010.
+//
+
+#import "ManifestItemTableView.h"
+
+
+@implementation ManifestItemTableView
+
+- (NSMenu *)menuForEvent:(NSEvent *)theEvent
+{
+	// get the current selections for the outline view. 
+	NSIndexSet *selectedRowIndexes = [self selectedRowIndexes];
+	
+	// select the row that was clicked before showing the menu for the event
+	NSPoint mousePoint = [self convertPoint:[theEvent locationInWindow] fromView:nil];
+	int row = [self rowAtPoint:mousePoint];
+	
+	// figure out if the row that was just clicked on is currently selected
+	if ([selectedRowIndexes containsIndex:row] == NO)
+	{
+		[self selectRowIndexes:[NSIndexSet indexSetWithIndex:row] byExtendingSelection:NO];
+	}
+	
+	return [super menuForEvent:theEvent];
+}
+
+@end
