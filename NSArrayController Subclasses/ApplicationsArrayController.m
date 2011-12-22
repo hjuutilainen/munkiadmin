c588e50cf8cc612e7a859c3732416d9d94ce121b
Hannes Juutilainen <hjuutilainen@mac.com>
2011-12-22 10:43:10 +0200

    Fixed sorting of grouped packages

--- NSArrayController Subclasses/ApplicationsArrayController.m
+++ NSArrayController Subclasses/ApplicationsArrayController.m
@@ -14,8 +14,9 @@
 {
 	[super awakeFromNib];
 	
-	NSSortDescriptor *sortByTitle = [[[NSSortDescriptor alloc] initWithKey:@"munki_display_name" ascending:YES selector:@selector(localizedStandardCompare:)] autorelease];
-	[self setSortDescriptors:[NSArray arrayWithObject:sortByTitle]];
+    NSSortDescriptor *sortByName = [NSSortDescriptor sortDescriptorWithKey:@"munki_name" ascending:YES selector:@selector(localizedStandardCompare:)];
+	NSSortDescriptor *sortByDisplayName = [NSSortDescriptor sortDescriptorWithKey:@"munki_display_name" ascending:YES selector:@selector(localizedStandardCompare:)];
+	[self setSortDescriptors:[NSArray arrayWithObjects:sortByName, sortByDisplayName, nil]];
 	
 	// Reload the table view when packages change
 	//[self addObserver:self forKeyPath:@"arrangedObjects.packages" options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld) context:nil];
