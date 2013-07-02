9b95024d03f314991d86b74d0c34a25333983fa1
Hannes Juutilainen <hjuutilainen@mac.com>
2013-07-02 13:05:40 +0300

    Items in manifests now try to resolve a correct version based on the manifest and catalog(s)

--- MunkiAdmin_DataModel/StringObjectMO.m
+++ MunkiAdmin_DataModel/StringObjectMO.m
@@ -2,6 +2,8 @@
 #import "PackageMO.h"
 #import "ApplicationMO.h"
 #import "ConditionalItemMO.h"
+#import "ManifestMO.h"
+#import "CatalogMO.h"
 
 @implementation StringObjectMO
 
@@ -75,23 +77,96 @@
     [self didChangeValueForKey:@"includedManifestConditionalReference"];
 }
 
+- (NSArray *)siblingPackagesWhenReferencedFromManifest:(ManifestMO *)manifest
+{
+    if (manifest == nil) {
+        return nil;
+    }
+    
+    NSArray *array = nil;
+    
+    /*
+     Get the catalog titles for this manifest
+     */
+    NSSet *enabledCatalogInfos = [manifest.catalogInfos filteredSetUsingPredicate:[NSPredicate predicateWithFormat:@"isEnabledForManifest == TRUE"]];
+    NSSet *enabledCatalogTitles = [enabledCatalogInfos valueForKeyPath:@"catalog.title"];
+    
+    /*
+     Now fetch the actual catalog objects
+     */
+    NSFetchRequest *catalogFetch = [NSFetchRequest new];
+    [catalogFetch setEntity:[NSEntityDescription entityForName:@"Catalog" inManagedObjectContext:self.managedObjectContext]];
+    [catalogFetch setPredicate:[NSPredicate predicateWithFormat:@"title IN %@", enabledCatalogTitles]];
+    if ([self.managedObjectContext countForFetchRequest:catalogFetch error:nil] > 0) {
+        NSArray *catalogs = [self.managedObjectContext executeFetchRequest:catalogFetch error:nil];
+        
+        /*
+         Get package proxy objects from catalog objects
+         */
+        NSMutableSet *mutableSet = [NSMutableSet setWithCapacity:[catalogs count]];
+        for (CatalogMO *catalog in catalogs) {
+            NSSet *enabledPackageInfos = [catalog.packageInfos filteredSetUsingPredicate:[NSPredicate predicateWithFormat:@"package.munki_name == %@ AND isEnabledForCatalog == TRUE", self.title]];
+            [mutableSet addObject:enabledPackageInfos];
+        }
+        
+        /*
+         Create a set containing the distinct package objects and sort the by version key
+         */
+        NSSet *unionSet = [mutableSet valueForKeyPath:@"@distinctUnionOfSets.package"];
+        NSSortDescriptor *byVersion = [NSSortDescriptor sortDescriptorWithKey:@"munki_version" ascending:NO selector:@selector(localizedStandardCompare:)];
+        array = [unionSet sortedArrayUsingDescriptors:[NSArray arrayWithObject:byVersion]];
+    }
+    
+    return array;
+}
+
 - (NSDictionary *)dictValue
 {
     NSString *subtitle;
-    NSUInteger numPkgs = [self.packagesWithSameTitle count];
-    if (numPkgs == 0) {
-        subtitle = @"";
-    } else {
-        NSSortDescriptor *byVersion = [NSSortDescriptor sortDescriptorWithKey:@"munki_version" ascending:NO selector:@selector(localizedStandardCompare:)];
-        NSArray *foundPkgs = [self.packagesWithSameTitle sortedArrayUsingDescriptors:[NSArray arrayWithObject:byVersion]];
-        NSString *latestVersion = [[foundPkgs objectAtIndex:0] munki_version];
-        if (numPkgs == 1) {
-            subtitle = [NSString stringWithFormat:@"%lu matching package (%@)", (unsigned long)numPkgs, latestVersion];
+    
+    ManifestMO *currentManifest = nil;
+    if (self.managedInstallReference) {
+        currentManifest = self.managedInstallReference;
+    } else if (self.managedUninstallReference) {
+        currentManifest = self.managedUninstallReference;
+    } else if (self.managedUpdateReference) {
+        currentManifest = self.managedUpdateReference;
+    } else if (self.optionalInstallReference) {
+        currentManifest = self.optionalInstallReference;
+    }
+    
+    /*
+     Set the subtitle if referenced from a manifest
+     */
+    NSArray *packages = [self siblingPackagesWhenReferencedFromManifest:currentManifest];
+    if (packages != nil) {
+        NSString *latestVersion = [[packages objectAtIndex:0] munki_version];
+        if ([packages count] == 1) {
+            subtitle = [NSString stringWithFormat:@"%lu matching package (%@)", (unsigned long)[packages count], latestVersion];
         } else {
-            subtitle = [NSString stringWithFormat:@"%lu matching packages (%@)", (unsigned long)numPkgs, latestVersion];
+            subtitle = [NSString stringWithFormat:@"%lu matching packages (%@)", (unsigned long)[packages count], latestVersion];
         }
     }
     
+    /*
+     Or get all packages with same title
+     */
+    else {
+        NSUInteger numPkgs = [self.packagesWithSameTitle count];
+        if (numPkgs == 0) {
+            subtitle = @"";
+        } else {
+            NSSortDescriptor *byVersion = [NSSortDescriptor sortDescriptorWithKey:@"munki_version" ascending:NO selector:@selector(localizedStandardCompare:)];
+            NSArray *foundPkgs = [self.packagesWithSameTitle sortedArrayUsingDescriptors:[NSArray arrayWithObject:byVersion]];
+            NSString *latestVersion = [[foundPkgs objectAtIndex:0] munki_version];
+            if (numPkgs == 1) {
+                subtitle = [NSString stringWithFormat:@"%lu matching package (%@)", (unsigned long)numPkgs, latestVersion];
+            } else {
+                subtitle = [NSString stringWithFormat:@"%lu matching packages (%@)", (unsigned long)numPkgs, latestVersion];
+            }
+        }
+    }
+
 	return [NSDictionary dictionaryWithObjectsAndKeys:
 			self.title, @"title",
             self.typeString, @"type",
