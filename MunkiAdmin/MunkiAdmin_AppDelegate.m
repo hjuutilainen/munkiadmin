b7f030a11a52b1ce6e946a0226de6df86172e69b
Hannes Juutilainen <hjuutilainen@mac.com>
2014-05-01 22:11:52 +0300

    MunkiAdmin will now present a warning if user tries to quit while the object context has pending changes.
    The main window edited state also tracks the same property.

--- MunkiAdmin/MunkiAdmin_AppDelegate.m
+++ MunkiAdmin/MunkiAdmin_AppDelegate.m
@@ -2853,49 +2853,31 @@
     before the application terminates.
  */
  
-- (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender {
-
-    if (!managedObjectContext) return NSTerminateNow;
-
-    if (![managedObjectContext commitEditing]) {
-        NSLog(@"%@:%@ unable to commit editing to terminate", [self class], NSStringFromSelector(_cmd));
-        return NSTerminateCancel;
-    }
-
-    if (![managedObjectContext hasChanges]) return NSTerminateNow;
-
-    NSError *error = nil;
-    if (![managedObjectContext save:&error]) {
-    
-        // This error handling simply presents error information in a panel with an 
-        // "Ok" button, which does not include any attempt at error recovery (meaning, 
-        // attempting to fix the error.)  As a result, this implementation will 
-        // present the information to the user and then follow up with a panel asking 
-        // if the user wishes to "Quit Anyway", without saving the changes.
-
-        // Typically, this process should be altered to include application-specific 
-        // recovery steps.  
-                
-        BOOL result = [sender presentError:error];
-        if (result) return NSTerminateCancel;
-
-        NSString *question = NSLocalizedString(@"Could not save changes while quitting.  Quit anyway?", @"Quit without saves error question message");
+- (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
+{
+    /*
+     This is not foolproof in any way but should catch most of the scenarios
+     */
+    if ([managedObjectContext hasChanges]) {
+        NSString *question = NSLocalizedString(@"Changes have not been saved yet. Quit anyway?", @"Quit without saves error question message");
         NSString *info = NSLocalizedString(@"Quitting now will lose any changes you have made since the last successful save", @"Quit without saves error question info");
         NSString *quitButton = NSLocalizedString(@"Quit anyway", @"Quit anyway button title");
         NSString *cancelButton = NSLocalizedString(@"Cancel", @"Cancel button title");
+        
         NSAlert *alert = [[NSAlert alloc] init];
         [alert setMessageText:question];
         [alert setInformativeText:info];
         [alert addButtonWithTitle:quitButton];
         [alert addButtonWithTitle:cancelButton];
-
-        NSInteger answer = [alert runModal];
-        alert = nil;
         
-        if (answer == NSAlertAlternateReturn) return NSTerminateCancel;
-
+        NSInteger answer = [alert runModal];
+        
+        if (answer == NSAlertSecondButtonReturn) {
+            return NSTerminateCancel;
+        } else {
+            return NSTerminateNow;
+        }
     }
-
     return NSTerminateNow;
 }
 
