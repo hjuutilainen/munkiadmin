a006a3a0258086cf6cf503f3e264270898d2c71a
Hannes Juutilainen <hjuutilainen@mac.com>
2015-03-06 15:25:27 +0200

    Added PXSourceList

--- Podfile
+++ Podfile
@@ -3,5 +3,6 @@ platform :osx, '10.8'
 target 'MunkiAdmin' do
 pod 'NSHash', '~> 1.0.1'
 pod 'CocoaLumberjack', '2.0.0-rc'
+pod 'PXSourceList', '~> 2.0'
 end
 
---
+++ Pods/Target Support Files/Pods-MunkiAdmin-PXSourceList/Pods-MunkiAdmin-PXSourceList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MunkiAdmin_PXSourceList : NSObject
+@end
+@implementation PodsDummy_Pods_MunkiAdmin_PXSourceList
+@end
