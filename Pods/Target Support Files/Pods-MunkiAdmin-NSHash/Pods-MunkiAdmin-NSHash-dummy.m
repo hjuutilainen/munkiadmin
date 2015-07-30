ff2b99fe3599700f40861167cf3a4ab3e151d8fe
Hannes Juutilainen <hjuutilainen@mac.com>
2015-07-30 08:33:01 +0300

    Updated all pods

--- Pods/Target Support Files/Pods-MunkiAdmin-NSHash/Pods-MunkiAdmin-NSHash-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MunkiAdmin_NSHash : NSObject
-@end
-@implementation PodsDummy_Pods_MunkiAdmin_NSHash
-@end
--- Pods/Target Support Files/Pods-MunkiAdmin-PXSourceList/Pods-MunkiAdmin-PXSourceList-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MunkiAdmin_PXSourceList : NSObject
-@end
-@implementation PodsDummy_Pods_MunkiAdmin_PXSourceList
-@end
