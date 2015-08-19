6325f49df8a9a3157c3c3fc89b760e9c9ce3754d
Hannes Juutilainen <hjuutilainen@mac.com>
2015-08-19 10:36:42 +0300

    Updated analytics permission message

--- MunkiAdmin/MAMunkiAdmin_AppDelegate.m
+++ MunkiAdmin/MAMunkiAdmin_AppDelegate.m
@@ -882,8 +882,8 @@ DDLogLevel ddLogLevel;
     if (![self.defaults boolForKey:@"analyticsPermissionAsked"]) {
         DDLogVerbose(@"User has not been asked about analytics. Presenting dialog...");
         NSAlert *askAnalyticsPermission = [[NSAlert alloc] init];
-        askAnalyticsPermission.messageText = @"Send anonymous usage data?";
-        askAnalyticsPermission.informativeText = @"MunkiAdmin uses DevMate and includes the option to send reports with anonymous usage information. This information is used solely to help understand how MunkiAdmin is used. The information sent to DevMate is not personally identifiable.";
+        askAnalyticsPermission.messageText = @"Send anonymous system profile?";
+        askAnalyticsPermission.informativeText = @"MunkiAdmin uses DevMate and includes the option to send anonymous system profile everytime the app is launched. The information sent to DevMate is not personally identifiable and does not include any information about your munki repository.\n\nDetails included in the profile:\n• MunkiAdmin version\n• OS X version and locale\n• CPU and GPU models\n• Machine model\n• The amount of RAM and free disk space\n\n";
         [askAnalyticsPermission addButtonWithTitle:@"Send"];
         [askAnalyticsPermission addButtonWithTitle:@"Don't Send"];
         NSInteger result = [askAnalyticsPermission runModal];
